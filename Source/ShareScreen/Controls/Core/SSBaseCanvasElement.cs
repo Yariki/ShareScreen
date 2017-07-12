b5f09b34f541354810203ee397071ac4eca6289f
Yariki <iyariki.ya@gmail.com>
2017-07-12 01:06:05 -0700

    added thumbs for draging and moving. added base canvas element.

---
+++ Source/ShareScreen/Controls/Core/SSBaseCanvasElement.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SS.ShareScreen.Controls.Core
+{
+    /// <summary>
+    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+    ///
+    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:SS.ShareScreen.Controls.Core"
+    ///
+    ///
+    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:SS.ShareScreen.Controls.Core;assembly=SS.ShareScreen.Controls.Core"
+    ///
+    /// You will also need to add a project reference from the project where the XAML file lives
+    /// to this project and Rebuild to avoid compilation errors:
+    ///
+    ///     Right click on the target project in the Solution Explorer and
+    ///     "Add Reference"->"Projects"->[Browse to and select this project]
+    ///
+    ///
+    /// Step 2)
+    /// Go ahead and use your control in the XAML file.
+    ///
+    ///     <MyNamespace:SSBaseCanvasElement/>
+    ///
+    /// </summary>
+    public class SSBaseCanvasElement : ContentControl,INotifyPropertyChanged
+    {
+
+        static SSBaseCanvasElement()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SSBaseCanvasElement), new FrameworkPropertyMetadata(typeof(SSBaseCanvasElement)));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public static readonly DependencyProperty IsSelectedProperty = DependencyProperty.Register(
+            "IsSelected", typeof(bool), typeof(SSBaseCanvasElement), new PropertyMetadata(default(bool)));
+
+        public bool IsSelected
+        {
+            get { return (bool) GetValue(IsSelectedProperty); }
+            set { SetValue(IsSelectedProperty, value); }
+        }
+        
+
+    }
+}
---
+++ Source/ShareScreen/Controls/Thumbs/SSDragThumb.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+
+namespace SS.ShareScreen.Controls.Thumbs
+{
+    public class SSDragThumb : Thumb
+    {
+        public SSDragThumb()
+        {
+            this.DragDelta += new DragDeltaEventHandler(this.DragThumb_DragDelta);
+        }
+
+        private void DragThumb_DragDelta(object sender, DragDeltaEventArgs e)
+        {
+            var dataContext = this.DataContext as FrameworkElement;
+            Canvas parent = VisualTreeHelper.GetParent((DependencyObject)dataContext) as Canvas;
+            if (dataContext == null || parent == null)
+                return;
+            double maxValue1 = double.MaxValue;
+            double maxValue2 = double.MaxValue;
+            double left = Canvas.GetLeft((UIElement)dataContext);
+            double top = Canvas.GetTop((UIElement)dataContext);
+            double num1 = double.IsNaN(left) ? 0.0 : Math.Min(left, maxValue1);
+            double num2 = double.IsNaN(top) ? 0.0 : Math.Min(top, maxValue2);
+            double num3 = Math.Max(-num1, e.HorizontalChange);
+            double num4 = Math.Max(-num2, e.VerticalChange);
+            double d1 = Canvas.GetLeft((UIElement)dataContext);
+            double d2 = Canvas.GetTop((UIElement)dataContext);
+            if (double.IsNaN(d1))
+                d1 = 0.0;
+            if (double.IsNaN(d2))
+                d2 = 0.0;
+            Canvas.SetLeft((UIElement)dataContext, d1 + num3);
+            Canvas.SetTop((UIElement)dataContext, d2 + num4);
+            parent.InvalidateMeasure();
+            e.Handled = true;
+        }
+    }
+}
